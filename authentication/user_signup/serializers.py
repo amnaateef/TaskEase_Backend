fc531bf8e1a0ee01b53941cf5d24b4ab3586fa0d
RidaFatima039 <rf5736374@gmail.com>
2025-05-28 16:01:17 +0500

    login added

--- authentication/user_signup/serializers.py
+++ authentication/user_signup/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import Expert, Customer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import check_password
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
 
 class ExpertSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +16,33 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+        user = None
+
+        try:
+            user = Expert.objects.get(email=email)
+        except Expert.DoesNotExist:
+            try:
+                user = Customer.objects.get(email=email)
+            except Customer.DoesNotExist:
+                raise serializers.ValidationError("Invalid email or password.")
+
+        if not check_password(password, user.password):
+            raise serializers.ValidationError("Invalid email or password.")
+
+        # Create JWT token
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "email": user.email,
+            "role": user.role,
+        }
